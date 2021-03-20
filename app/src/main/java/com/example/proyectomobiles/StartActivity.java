03fa163ca2f9fd8c667abceeb53bcfd0b6c4b0cc
Arix23 <arivalenzuela23@gmail.com>
2021-03-20 15:55:15 -0700

    firebase basics and redirection buttons

--- app/src/main/java/com/example/proyectomobiles/StartActivity.java
+++ app/src/main/java/com/example/proyectomobiles/StartActivity.java
@@ -2,7 +2,9 @@ package com.example.proyectomobiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void openLogin(View v){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void openRegister(View v){
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
     }
 }
