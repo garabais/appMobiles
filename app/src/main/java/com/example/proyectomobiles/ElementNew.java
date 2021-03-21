718e9edb841a45b5db10db73b9fbcc4b063331a7
Arix23 <arivalenzuela23@gmail.com>
2021-03-20 20:38:02 -0700

    cambios de formato

--- app/src/main/java/com/example/proyectomobiles/ElementNew.java
+++ app/src/main/java/com/example/proyectomobiles/ElementNew.java
@@ -2,13 +2,32 @@ package com.example.proyectomobiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ElementNew extends AppCompatActivity {
+
+    private EditText puntaje, nombreElemento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_element_new);
+        puntaje = findViewById(R.id.puntuacionElemento);
+        nombreElemento = findViewById(R.id.nombreElemento);
+    }
+
+    public void limpiarFormato(View v){
+        puntaje.setText("");
+        nombreElemento.setText("");
+    }
+
+    public void cancelar(View v){
+        Intent i = new Intent();
+        setResult(Activity.RESULT_CANCELED,i);
+        finish();
     }
 }
