448a80cd6db7514600bd096631c52a42b8f5e7ef
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2021-03-20 20:26:46 -0600

    Username saved on firebase and displayed on landing page

--- app/src/main/java/com/example/proyectomobiles/LandingPageActivity.java
+++ app/src/main/java/com/example/proyectomobiles/LandingPageActivity.java
@@ -3,12 +3,46 @@ package com.example.proyectomobiles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LandingPageActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private TextView userTV;
+
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        userTV = findViewById(R.id.user);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // test to see if user is logged in
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null)
+        {
+            // do something if its logged in (or not!)
+            Log.d("USER", "display name: " + user.getDisplayName());
+            Log.d("USER", "email: " + user.getEmail());
+
+           username = user.getDisplayName();
+           userTV.setText(username);
+        }
+
+
+    }
+
+
 }
